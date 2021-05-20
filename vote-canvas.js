dbd19728f0f01af3c6c1aefbb539e4ba342edf46
sergevdokimov20 <84476220+sergevdokimov20@users.noreply.github.com>
2021-05-20 23:22:20 +0300

    Update vote-canvas.js

--- vote-canvas.js
+++ vote-canvas.js
@@ -1,6 +1,23 @@
 {
   const canvas = document.getElementById("vote-canvas");
   const ctx = canvas.getContext("2d");
+  let dpi = window.devicePixelRatio;
+  function fix_dpi() {
+    //get CSS height
+    //the + prefix casts it to an integer
+    //the slice method gets rid of "px"
+    let style_height = +getComputedStyle(canvas)
+      .getPropertyValue("height")
+      .slice(0, -2);
+    //get CSS width
+    let style_width = +getComputedStyle(canvas)
+      .getPropertyValue("width")
+      .slice(0, -2);
+    //scale the canvas
+    canvas.setAttribute("height", style_height * dpi);
+    canvas.setAttribute("width", style_width * dpi);
+  }
+  fix_dpi();
   const img = new Image();
   const imageUrl = "https://dycover.ru/render/204000980/160595";
 
